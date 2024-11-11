9fcd6826e93247f95190716484e0abcbb3eef2cd
HishamAbualhaj <hishamecommerce@gmail.com>
2024-11-11 17:10:17 +0200

    Desing Demo page + footer ,still in progress

--- src/components/Landing-Page/DemoPage.jsx
+++ src/components/Landing-Page/DemoPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
 function DemoPage() {
   return (
-    <div>DemoPage</div>
-  )
+    <div className="section bg-lightLayout dark:bg-lightDark py-12">
+      <div className="max-container ">
+        <div className="flex justify-between">
+          <div>
+            <div className="font-[600] lg:text-4xl text-xl">Our Platform</div>
+            <div className="text-black/50 max-w-[600px] dark:text-white/50 mt-2 leading-7">
+              Our platform is designed to make learning effortless and
+              inspiring, with personalized recommendations, interactive content,
+              and 24/7 access to courses crafted by top industry experts.
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Button outlined={true} text="Join a course" size="xl" url={""} />
+            <Button outlined={false} text="Sign up" size="xl" url={""} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DemoPage
+export default DemoPage;
--- src/components/Landing-Page/Footer.jsx
+++ src/components/Landing-Page/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../Logo";
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="section">
+      <div className="max-container">
+          <Logo />
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
