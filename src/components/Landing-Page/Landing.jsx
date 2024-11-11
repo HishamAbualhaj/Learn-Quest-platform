d5eac88a9169b50cea504498121d4b29f30a02df
HishamAbualhaj <hishamecommerce@gmail.com>
2024-11-11 13:41:17 +0200

    Setting up landing components

--- src/components/Landing-Page/Landing.jsx
+++ src/components/Landing-Page/Landing.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Header from "./Header";
+import Hero from "./Hero";
+import Feature from "./Feature";
+import FeatureDetails from "./FeatureDetails";
+import Testimonials from "./Testimonials";
+import CoursePreview from "./CoursePreview";
+import DemoPage from "./DemoPage";
+import Footer from "./Footer";
 function Landing() {
   return (
     <>
       <Header />
+      <Hero />
+      <Feature />
+      <FeatureDetails />
+      <Testimonials />
+      <CoursePreview />
+      <DemoPage />
+      <Footer />
     </>
   );
 }
