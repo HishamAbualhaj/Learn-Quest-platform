9e0b9a7c16936fe7a208d9b766bd42a8edff2599
HishamAbualhaj <hishamecommerce@gmail.com>
2024-11-11 16:12:21 +0200

    Change hover button

--- src/components/Landing-Page/Button.jsx
+++ src/components/Landing-Page/Button.jsx
@@ -17,13 +17,13 @@ function Button({ outlined, size = "px-4 py-2", text, url }) {
     <>
       {outlined ? (
         <div
-          className={`${currentSize} cursor-pointer w-fit border rounded-md dark:border-mainClrDark border-lightBtn text-mainClr dark:text-white font-[600] hover:bg-mainClrDark hover:text-black hover:border-mainClrDark transition`}
+          className={`${currentSize} cursor-pointer w-fit border rounded-md dark:border-mainClrDark border-lightBtn text-mainClr dark:text-white font-[600] hover:bg-mainClrDark hover:text-white hover:border-mainClrDark transition`}
         >
           <a href={url}>{text}</a>
         </div>
       ) : (
         <div
-          className={`${currentSize} cursor-pointer w-fit rounded-md border dark:bg-mainClrDark dark:border-mainClrDark border-mainClr bg-mainClr dark:text-black text-white  font-[600] transition hover:bg-mainClrDark hover:text-black hover:border-mainClrDark`}
+          className={`${currentSize} cursor-pointer w-fit rounded-md border dark:bg-mainClrDark dark:border-mainClrDark border-mainClr bg-mainClr dark:text-black text-white  font-[600] transition hover:bg-mainClrDark hover:text-white hover:border-mainClrDark`}
         >
           <a href={url}> {text}</a>
         </div>
