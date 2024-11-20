ee3bc377601da3e78c50fd559adb9bd8f60a6a51
HishamAbualhaj <hishamecommerce@gmail.com>
2024-11-20 20:35:18 +0200

    Solve an issue with button data

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-
 function Button({
   text,
   textClr = "text-white",
   textDarkClr,
   bgClr = "bg-purple-600",
-  bgDarkClr = "bg-purple-700/40",
+  bgDarkClr = "dark:bg-purple-700/40",
   hoverTextClr,
-  hoverBgClr = "bg-purple-700",
+  hoverBgClr = "hover:bg-purple-700",
   hoverDarkTextClr,
-  hoverDarkBgClr = "bg-purple-700",
+  hoverDarkBgClr = "dark:hover:bg-purple-700",
   padding = "p-2",
   margin = "mt-4",
-  props,
+  props = "",
 }) {
   return (
     <div
-      className={`btn ${padding} ${props} ${margin} rounded-md dark:${bgDarkClr} w-fit cursor-pointer dark:hover:${hoverDarkBgClr} ${bgClr} ${textClr} hover:${hoverBgClr} transition`}
+      className={`${padding} ${props} ${margin} rounded-md ${bgDarkClr} w-fit cursor-pointer ${hoverDarkBgClr} ${bgClr} ${textClr} ${hoverBgClr} transition`}
     >
       {text}
     </div>
