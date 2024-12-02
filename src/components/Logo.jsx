06f10afb851e0f94a8903df9053f934a02558635
Hisham Abualhaj <hishamecommerce@gmail.com>
2024-12-02 09:52:45 +0200

    Making logo redirect to the landing page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Logo({ textSize = "text-2xl" }) {
   return (
-    <div
-      className={`dark:text-white text-black ${textSize} py-[18px] font-bold flex gap-1`}
-    >
-      LEARN <div className="text-purple-600">QUEST</div>
-    </div>
+    <Link to="/">
+      <div
+        className={`dark:text-white text-black ${textSize} py-[18px] font-bold flex gap-1`}
+      >
+        LEARN <div className="text-purple-600">QUEST</div>
+      </div>
+    </Link>
   );
 }
