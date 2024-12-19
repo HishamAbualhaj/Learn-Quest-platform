16f431d09ab2a5370fcf3a115dfa8ca3d2c7fc60
HishamAbualhaj <hishamecommerce@gmail.com>
2024-12-19 16:22:41 +0200

    Adjust alert component for light mode

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Alert({ msg, type }) {
   return type === "success" ? (
-    <div className="bg-green-900/40 text-green-400 py-3 pl-5 rounded-md text-xl relative mb-2">
-      <div className="absolute left-0 top-0 h-full w-3 bg-green-900 rounded-tl-md rounded-bl-md"></div>
+    <div className="dark:bg-green-900/40 bg-green-200 dark:text-green-400 text-green-800 py-3 pl-5 rounded-md text-xl relative mb-2">
+      <div className="absolute left-0 top-0 h-full w-3 dark:bg-green-900 bg-green-500/80 rounded-tl-md rounded-bl-md"></div>
       {msg}
     </div>
   ) : (
-    <div className="bg-red-900/40 text-red-400 py-3 pl-5 rounded-md text-xl relative mb-2">
-      <div className="absolute left-0 top-0 h-full w-3 bg-red-900 rounded-tl-md rounded-bl-md"></div>
+    <div className="dark:bg-red-900/40 bg-red-200 dark:text-red-400 text-red-800 py-3 pl-5 rounded-md text-xl relative mb-2">
+      <div className="absolute left-0 top-0 h-full w-3 dark:bg-red-900 bg-red-500/80 rounded-tl-md rounded-bl-md"></div>
       {msg}
     </div>
   );
