0a1cd29a9fe5ac7339a106b03f15467ff2e16d74
Hisham Abualhaj <hishamecommerce@gmail.com>
2024-12-20 19:37:32 +0200

    Sepreate function from sign up for later use

---
+++ back-end/utils/isEmailFound.js
@@ -0,0 +1,36 @@
+async function isEmailFound(email) {
+  try {
+    const query = `SELECT * from user WHERE email = ?`;
+    const result = await connection
+      .promise()
+      .query(query, [email], (err) => {
+        handleResponse(
+          response,
+          err,
+          "Error at selecting data : ",
+          201,
+          500,
+          "",
+          "Something went wrong"
+        );
+      })
+      .then((data) => {
+        return data;
+      });
+    const [data] = result;
+    console.log("Current data is ", data);
+    return data.length === 0 ? false : true;
+  } catch (error) {
+    handleResponse(
+      response,
+      error,
+      "Error Internal server at isEmailFound : ",
+      201,
+      500,
+      "",
+      "Something went wrong"
+    );
+  }
+}
+
+export default isEmailFound;
