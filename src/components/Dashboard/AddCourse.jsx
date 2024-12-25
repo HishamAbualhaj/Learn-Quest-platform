257bf408638e37c6cc8deacd0bf509c7a0d24fcd
Hisham Abualhaj <hishamecommerce@gmail.com>
2024-12-25 13:49:42 +0200

    Handle state for course and lesson inputs

--- src/components/Dashboard/AddCourse.jsx
+++ src/components/Dashboard/AddCourse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
@@ -7,24 +7,97 @@ export default function AddCourse() {
   const inputs = [
     {
       key: 1,
+      id: "title",
       title: "Title : ",
       inType: "text",
+      placeholder: "Course Title Here",
     },
     {
       key: 2,
+      id: "price",
       title: "Price : ",
       inType: "number",
+      placeholder: "50",
     },
     {
       key: 3,
+      id: "discount",
       title: "Discount : ",
       inType: "number",
+      placeholder: "20%",
+    },
+    {
+      key: 4,
+      id: "tabs",
+      title: "Tabs : ",
+      inType: "text",
+      placeholder: "UI/UX FRONT-END etc ...",
     },
   ];
 
+  const [courseData, setCourseData] = useState({
+    title: "",
+    price: "",
+    discount: "",
+    category: "",
+    image: "",
+    description: "",
+    tabs: [""],
+    materials: [
+      {
+        title: "",
+        subtitle: "",
+        url: "",
+      },
+    ],
+  });
+
+  useEffect(() => {
+    console.log(courseData);
+  }, [courseData]);
+
+  function handleChange(e) {
+    let { id, value } = e.target;
+    if (id === "image") {
+      value = e.target.files[0]?.name;
+    }
+    if(id === 'tabs') {
+      value = value.split(' ')
+    }
+    setCourseData({
+      ...courseData,
+      [id]: value,
+    });
+  }
+  // handle lesson change
+  function handleLessonChange(e, lesson_id) {
+    const { id, value } = e.target;
+    const updateMaterial = [...courseData.materials];
+    // update prev value for each lesson
+    updateMaterial[lesson_id - 1] = {
+      ...updateMaterial[lesson_id - 1],
+      [id]: value,
+    };
+    setCourseData({
+      ...courseData,
+      materials: updateMaterial,
+    });
+  }
   const [lessons, setLessons] = useState([1]);
   function addLesson() {
     setLessons([...lessons, lessons.at(-1) + 1]);
+    // add new object value for new lesson
+    setCourseData({
+      ...courseData,
+      materials: [
+        ...courseData.materials,
+        {
+          title: "",
+          subtitle: "",
+          url: "",
+        },
+      ],
+    });
   }
   const category = [
     { id: 1, name: "Introduction to Computer Science" },
@@ -64,8 +137,14 @@ export default function AddCourse() {
               key={input.key}
               className="flex flex-col mt-2 dark:text-white text-lightText"
             >
-              <label htmlFor="">{input.title}</label>
-              <input className="mt-2" type={input.inType} />
+              <label htmlFor={input.id}>{input.title}</label>
+              <input
+                onChange={handleChange}
+                placeholder={input.placeholder}
+                id={input.id}
+                className="mt-2"
+                type={input.inType}
+              />
             </div>
           ))}
 
@@ -73,9 +152,10 @@ export default function AddCourse() {
             <label htmlFor="">Category : </label>
 
             <select
+              onChange={handleChange}
               className="bg-transparent border dark:border-[#888] border-borderLight rounded-sm dark:text-white text-lightText focus:outline-none p-2 text-lg appearance-none w-full mt-2"
               name="courses"
-              id=""
+              id="category"
             >
               {category.map((category) => (
                 <option
@@ -91,12 +171,14 @@ export default function AddCourse() {
           </div>
 
           <div className="flex flex-col mt-2 dark:text-white text-lightText ">
-            <label htmlFor="">Image : </label>
+            <label htmlFor="image">Image : </label>
 
             <div className="image-handle relative cursor-pointer mt-2">
               <div className="border_platform all rounded-md flex justify-center py-10">
                 <div className="text-xl "> Upload image</div>
                 <input
+                  id="image"
+                  onChange={handleChange}
                   type="file"
                   className="rounded-md w-full h-full opacity-0 absolute top-0 left-0 cursor-pointer"
                 />
@@ -129,6 +211,10 @@ export default function AddCourse() {
                     <div className="flex flex-col gap-2">
                       <div>Title: </div>
                       <input
+                        onChange={(e) => {
+                          handleLessonChange(e, id);
+                        }}
+                        id="title"
                         className="w-full"
                         type="text"
                         placeholder="Lessson title"
@@ -139,6 +225,10 @@ export default function AddCourse() {
                     <div className="flex flex-col gap-2">
                       <div>Sub Title: </div>
                       <input
+                        onChange={(e) => {
+                          handleLessonChange(e, id);
+                        }}
+                        id="subtitle"
                         className="w-full"
                         type="text"
                         placeholder="Lessson sub title"
@@ -149,6 +239,10 @@ export default function AddCourse() {
                     <div className="flex flex-col gap-2">
                       <div>Lesson Url: </div>
                       <input
+                        onChange={(e) => {
+                          handleLessonChange(e, id);
+                        }}
+                        id="url"
                         className="w-full"
                         type="text"
                         placeholder="Lessson url"
@@ -161,8 +255,12 @@ export default function AddCourse() {
           </div>
 
           <div className="flex flex-col mt-2 dark:text-white text-lightText">
-            <label htmlFor="">Description : </label>
-            <textarea className="mt-2" />
+            <label htmlFor="description">Description : </label>
+            <textarea
+              id="description"
+              onChange={handleChange}
+              className="mt-2"
+            />
           </div>
           <ButtonAdmin text="ADD" />
         </div>
