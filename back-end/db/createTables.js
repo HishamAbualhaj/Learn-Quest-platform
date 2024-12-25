b404047c31e0c093633ddc50f8927059b82e5da3
Hisham Abualhaj <hishamecommerce@gmail.com>
2024-12-25 13:49:15 +0200

    Create course material table and edit schemas

--- back-end/db/createTables.js
+++ back-end/db/createTables.js
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS User (
   password VARCHAR(50) NOT NULL,
   gender VARCHAR(10),
   birthdate DATE,
-  joined_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 `;
 
@@ -32,17 +32,28 @@ CREATE TABLE IF NOT EXISTS Courses (
   duration TIME NOT NULL,
   category VARCHAR(50),
   tabs TEXT,
-  created_date DATETIME DEFAULT CURRENT_TIMESTAMP
+  created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 `;
-
+const createCoursesMaterials = `
+CREATE TABLE IF NOT EXISTS CourseMaterials (
+  material_id INT AUTO_INCREMENT PRIMARY KEY,
+  course_id INT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  subtitle VARCHAR(100) NOT NULL,
+  isCompleted BOOLEAN DEFAULT FALSE,
+  url VARCHAR(2083),
+  created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,c
+  FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
+`;
 const createSystemLogsTable = `
 CREATE TABLE IF NOT EXISTS SystemLogs (
   log_id INT AUTO_INCREMENT PRIMARY KEY,
   student_id INT,
   message TEXT NOT NULL,
   email VARCHAR(100),
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (student_id) REFERENCES User(student_id)
 );
 `;
@@ -52,8 +63,8 @@ CREATE TABLE IF NOT EXISTS session (
     session_id VARCHAR(255) NOT NULL PRIMARY KEY,  
     user_id INT NOT NULL,                       
     data TEXT,                                    
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    expires_at DATETIME NOT NULL                  
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    expires_at TIMESTAMP NOT NULL                  
 );`;
 // Execute the queries
 const tables = [
@@ -61,7 +72,8 @@ const tables = [
   createUserTable,
   createCoursesTable,
   createSystemLogsTable,
-  createSessionTable
+  createSessionTable,
+  createCoursesMaterials,
 ];
 
 tables.forEach((query) => {
