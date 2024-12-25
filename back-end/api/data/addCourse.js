aa8bebd06a5e5f51adef670bf2e595d890f4082a
zam <monal.75757@gmail.com>
2024-12-25 15:28:40 +0200

    Handle server side adding course done

--- back-end/api/data/addCourse.js
+++ back-end/api/data/addCourse.js
@@ -1,11 +1,11 @@
 import connection from "../../db/db.js";
 import handleResponse from "../../utils/handleResponse.js";
-import log from "../../system/logs.js";
+
 const addCourse = (req, res) => {
+  let body = "";
   // Triggering received data from client and collect it
   req.on("data", (chunks) => {
     body += chunks.toString();
-    console.log(body);
   });
 
   // Entire body has been received : no more data is coming
@@ -17,27 +17,34 @@ const addCourse = (req, res) => {
         discount,
         category,
         image,
+        tabs,
         description,
         materials,
       } = JSON.parse(body);
-      console.log(materials);
-      const query =
-        `  INSERT INTO Courses (course_id,title, description, price, discount, category, tabs, image_url)
-    VALUES (?, ?, ?, ?, ?, ?,?)`(async () => {
-        
-          await connection
-            .promise()
-            .query(query, [
-              course_id,
-              title,
-              description,
-              price,
-              discount,
-              category,
-              tabs,
-              image,
-            ]);
-        });
+
+      (async () => {
+        const course_id = Math.round(Math.random() * 100000000);
+        await insertCourse(
+          course_id,
+          title,
+          price,
+          discount,
+          category,
+          image,
+          tabs,
+          description,
+          materials
+        );
+        handleResponse(
+          res,
+          null,
+          "",
+          201,
+          500,
+          "Course added successfully !",
+          ""
+        );
+      })();
     } catch (error) {
       handleResponse(
         res,
@@ -50,5 +57,44 @@ const addCourse = (req, res) => {
     }
   });
 };
+async function insertCourse(
+  course_id,
+  title,
+  price,
+  discount,
+  category,
+  image,
+  tabs,
+  description,
+  materials
+) {
+  tabs = tabs.toString();
+  const query = `INSERT INTO Courses (course_id,title, description, price, discount, category, tabs, image_url)
+    VALUES (?, ?, ?, ?, ?, ?,?,?)`;
 
+  await connection
+    .promise()
+    .query(query, [
+      course_id,
+      title,
+      description,
+      price,
+      discount,
+      category,
+      tabs,
+      image,
+    ]);
+  await insertCourseMaterial(materials, course_id);
+}
+async function insertCourseMaterial(data, course_id) {
+  const query = `INSERT INTO coursematerials (material_id,course_id,title,subtitle,url)
+  VALUES (?, ?, ?, ?, ?)`;
+  for (const obj of data) {
+    const material_id = Math.round(Math.random() * 100000000);
+    const { title, subtitle, url } = obj;
+    await connection
+      .promise()
+      .query(query, [material_id, course_id, title, subtitle, url]);
+  }
+}
 export default addCourse;
