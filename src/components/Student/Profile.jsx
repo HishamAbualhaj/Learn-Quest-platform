c3604420be98ffcb68d7f943d23a58eb5ef9e5df
HishamAbualhaj <hishamecommerce@gmail.com>
2024-12-27 18:44:09 +0200

    Trying to handle user update profile

--- src/components/Student/Profile.jsx
+++ src/components/Student/Profile.jsx
@@ -1,48 +1,141 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Avatar from "../Avatar";
 import hisham from "../../assets/Screenshot_1.jpg";
 import { faGear, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const profile = [
-  {
-    key: 1,
-    name: "First name",
-    data: "Hisham",
-  },
-  {
-    key: 2,
-    name: "Last name",
-    data: "Abualhaj",
-  },
-  {
-    key: 3,
-    name: "Email",
-    data: "hisham.raid@yahoo.com",
-  },
-  {
-    key: 4,
-    name: "Birth Of Date",
-    data: "14/08/2001",
-  },
-  {
-    key: 5,
-    name: "Gender",
-    data: "Male",
-  },
-  {
-    key: 6,
-    name: "Joined At",
-    data: "11/15/2024 7:44pm",
-  },
-  {
-    key: 7,
-    name: "Course Completed",
-    data: "5",
-  },
-];
+import useFetch from "../Hooks/useFetch";
+
 function Profile() {
   const [state, setState] = useState("Profile");
   const [isEdit, setIsEdit] = useState(false);
+  const [profileData, setProfileData] = useState([
+    {
+      key: 1,
+      name: "First name",
+      data: "first_name",
+    },
+    {
+      key: 2,
+      name: "Last name",
+      data: "last_name",
+    },
+    {
+      key: 3,
+      name: "Email",
+      data: "email",
+    },
+    {
+      key: 4,
+      name: "Birth Of Date",
+      data: "date",
+    },
+    {
+      key: 5,
+      name: "Gender",
+      data: "gender",
+    },
+    {
+      key: 6,
+      name: "Joined At",
+      data: "join",
+    },
+    {
+      key: 7,
+      name: "Course Completed",
+      data: "5",
+    },
+  ]);
+  const [data, setData] = useState({});
+  useEffect(() => {
+    (async function fetchData() {
+      const response = await useFetch(
+        "http://localhost:3002/session",
+        null,
+        "GET"
+      );
+      let id = response.msg.userId;
+
+      // after getting user id then fetch for data
+      const user = {
+        id: id,
+      };
+      (async () => {
+        const res = await useFetch(
+          "http://localhost:3002/getUserData",
+          user,
+          "POST"
+        );
+        console.log(res);
+        if (res.msg.data) {
+          const [
+            { first_name, last_name, email, gender, joined_at, birthdate },
+          ] = res.msg.data;
+
+          const date = birthdate.split("T")[0];
+          const join = joined_at.split("T")[0];
+          setData({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            gender: gender,
+            joined_at: join,
+            birthdate: date,
+          });
+          // const student_data = [
+          // {
+          //   key: 1,
+          //   name: "First name",
+          //   data: first_name,
+          // },
+          // {
+          //   key: 2,
+          //   name: "Last name",
+          //   data: last_name,
+          // },
+          // {
+          //   key: 3,
+          //   name: "Email",
+          //   data: email,
+          // },
+          // {
+          //   key: 4,
+          //   name: "Birth Of Date",
+          //   data: date,
+          // },
+          // {
+          //   key: 5,
+          //   name: "Gender",
+          //   data: gender,
+          // },
+          // {
+          //   key: 6,
+          //   name: "Joined At",
+          //   data: join,
+          // },
+          // {
+          //   key: 7,
+          //   name: "Course Completed",
+          //   data: "5",
+          // },
+          // ];
+          // setProfileData(student_data);
+        }
+      })();
+    })();
+  }, []);
+  function handleChange(e) {
+    const { id, value } = e.target;
+
+    // setProfileData();
+    console.log("Updated profile data: ", value);
+    // setProfileData(updatedProfileData);
+
+    console.log(id, value);
+  }
+
+  useEffect(() => {
+    console.log(profileData);
+  }, [profileData]);
   return (
     <div className="sm:px-5 px-1 flex items-center height-vh-adjust">
       <div className=" dark:bg-lightDark w-full bg-white border dark:border-borderDark flex items-center flex-col  box-shadow-light dark:box-shadow rounded-md h-[800px] overflow-auto">
@@ -71,7 +164,7 @@ function Profile() {
         </div>
         <div className="flex w-full flex-col gap-5 py-16 lg:px-16 md:px-8 sm:px-4 px-2">
           {state === "Profile" ? (
-            profile.map((data) => (
+            profileData.map((data) => (
               <div
                 key={data.key}
                 className="text-xl flex gap-2 border dark:border-borderDark p-4 rounded-md"
@@ -91,27 +184,29 @@ function Profile() {
       </div>
     </div>
   );
+
+  function EditProfile() {
+    return (
+      <>
+        {profileData.map((obj) => (
+          <div
+            key={obj.key}
+            className="text-xl items-center flex gap-2 border dark:border-borderDark p-4 rounded-md"
+          >
+            {obj.name}:
+            <div className="text-slate-300 flex-1">
+              <input
+                id={obj.key}
+                onChange={handleChange}
+                className="border dark:border-borderDark w-full"
+                type="text"
+              />
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
-function EditProfile() {
-  return (
-    <>
-      {profile.map((data) => (
-        <div
-          key={data.key}
-          className="text-xl items-center flex gap-2 border dark:border-borderDark p-4 rounded-md"
-        >
-          {data.name}:{" "}
-          <div className="text-slate-300 flex-1">
-            <input
-              value={data.data}
-              className="border dark:border-borderDark w-full"
-              type="text"
-            />
-          </div>
-        </div>
-      ))}
-    </>
-  );
-}
 export default Profile;
