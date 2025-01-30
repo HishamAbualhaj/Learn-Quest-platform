f02945c633362f531bf808ed32bfa1c40cc58fc3
HishamAbualhaj <hishamecommerce@gmail.com>
2025-01-30 17:42:06 +0200

    Handle header authentication and dropdown menu activeness

--- src/components/Landing-Page/Landing.jsx
+++ src/components/Landing-Page/Landing.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useFetch from "../Hooks/useFetch";
 import Header from "./Header";
 import Hero from "./Hero";
 import Feature from "./Feature";
 import FeatureDetails from "./FeatureDetails";
 import Testimonials from "./Testimonials";
 import CoursePreview from "./CoursePreview";
-import FrequentlyQuestions from './FrequentlyQuestions'
+import FrequentlyQuestions from "./FrequentlyQuestions";
 import DemoPage from "./DemoPage";
 import Footer from "./Footer";
+import { useLoaderData } from "react-router-dom";
 function Landing() {
+  const userDataResponse = useLoaderData();
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    setUserData(userDataResponse);
+  }, []);
+
+  const [activeDrop, setActiveDrop] = useState(false);
+  useEffect(() => {
+    setActiveDrop(activeDrop);
+  }, [activeDrop]);
   return (
-    <>
-      <Header />
-      <Hero />
+    <div
+      onClick={() => {
+        setActiveDrop(false);
+      }}
+    >
+      <Header
+        data={userData}
+        sendData={setActiveDrop}
+        activeDrop={activeDrop}
+      />
+      <Hero data={userData} />
       <Feature />
       <FeatureDetails />
       <Testimonials />
@@ -20,7 +40,7 @@ function Landing() {
       <FrequentlyQuestions />
       <DemoPage />
       <Footer />
-    </>
+    </div>
   );
 }
 export default Landing;
