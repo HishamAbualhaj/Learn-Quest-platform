d8ceb11b7554ab59b8618d5a0edbe06060f94d05
HishamAbualhaj <hishamecommerce@gmail.com>
2025-02-07 18:45:23 +0200

    Delete user from db server side

--- back-end/api/data/deleteUser.js
+++ back-end/api/data/deleteUser.js
@@ -1,22 +1,31 @@
 import connection from "../../db/db.js";
 import handleResponse from "../../utils/handleResponse.js";
-
+import archiveLog from "../../system/archiveLogs.js";
 const deleteUser = (req, res) => {
   let body = "";
   req.on("data", (chunks) => {
     body += chunks;
   });
   req.on("end", () => {
-    const { course_id } = JSON.parse(body);
-    deleteUserQ(course_id, res);
+    const { user_id } = JSON.parse(body);
+    deleteUserQ(user_id, res);
   });
 };
 
-async function deleteUserQ(course_id, res) {
+async function deleteUserQ(user_id, res) {
   try {
     const query = "DELETE FROM user WHERE student_id = ?";
-    await connection.promise().query(query, [course_id]);
-
+    const get_name_query = `SELECT first_name from user WHERE student_id = ?`;
+    const [data] = await connection.promise().query(get_name_query, [user_id]);
+    const [{ first_name }] = data;
+    await archiveLog(
+      res,
+      user_id,
+      "USER",
+      `Admin: Deleted User ${first_name}`,
+      email
+    );
+    await connection.promise().query(query, [user_id]);
     handleResponse(
       res,
       null,
@@ -29,10 +38,11 @@ async function deleteUserQ(course_id, res) {
       },
       ""
     );
+    return;
   } catch (error) {
-    handleResponse(null, error, "Error Deleting user: ", 201, 500, "", {
+    handleResponse(res, error, "Error Deleting user: ", 201, 500, "", {
       isDeleted: false,
-      msg: "User deleted Successfully, ",
+      msg: "User deletion failed, ",
     });
   }
 }
