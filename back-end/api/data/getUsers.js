dede30809ed8d9af2d7756c999cdd567f49901b8
HishamAbualhaj <hishamecommerce@gmail.com>
2025-05-01 21:18:22 +0300

    Get users for admin , infinite scroll

--- back-end/api/data/getUsers.js
+++ back-end/api/data/getUsers.js
@@ -1,21 +1,34 @@
 import connection from "../../db/db.js";
 import handleResponse from "../../utils/handleResponse.js";
-const getUsers = async (_, res) => {
-  try {
-    const query = `SELECT * FROM user`;
-    const result = await connection.promise().query(query);
-    handleResponse(res, null, "", 201, 500, result[0], "");
-  } catch (error) {
-    handleResponse(
-      res,
-      error,
-      "Error selecting from users: ",
-      null,
-      500,
-      null,
-      "Error to select user"
-    );
-  }
+const getUsers = async (req, res) => {
+  let body = "";
+  req.on("data", (chunks) => {
+    body += chunks;
+  });
+
+  req.on("end", async () => {
+    try {
+      const { page } = JSON.parse(body);
+      const query = `SELECT student_id,first_name,last_name,status_user,email,gender,birthdate,role,image_url,joined_at FROM user WHERE role != 'admin' ORDER BY joined_at DESC LIMIT ? OFFSET ?`;
+      const result = await connection
+        .promise()
+        .query(query, [10, page === 1 ? 0 : (page - 1) * 5]);
+
+      result[0].length === 0
+        ? handleResponse(res, null, null, 200, null, result[0], null, null)
+        : handleResponse(res, null, null, 200, null, result[0], null, page + 1);
+    } catch (error) {
+      handleResponse(
+        res,
+        error,
+        "Error selecting from users: ",
+        null,
+        500,
+        null,
+        "Error to select user"
+      );
+    }
+  });
 };
 
 export default getUsers;
