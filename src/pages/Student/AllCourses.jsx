8b97ffe05e3c6e4c7c7a9d8edf7de6cd535014a6
HishamAbualhaj <hishamecommerce@gmail.com>
2025-05-01 21:16:32 +0300

    Filter courses by search and select , for both infinite scroll and pagination

--- src/pages/Student/AllCourses.jsx
+++ src/pages/Student/AllCourses.jsx
@@ -1,80 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faStar } from "@fortawesome/free-solid-svg-icons";
 import Button from "../../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import Loader from "../../components/Loader";
 import useFetch from "../../hooks/useFetch";
-import { useQuery } from "@tanstack/react-query";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import API_BASE_URL from "../../config/config";
 function AllCourses() {
-  const { data, isLoading } = useQuery({
-    queryFn: async () => {
-      return await useFetch("http://localhost:3002/getCourses", null, "GET");
-    },
-    queryKey: ["courses"],
+  const [searchPara, setSearchPara] = useSearchParams();
+  const [fetchData, setFetchData] = useState({
+    search_text: searchPara.get("search") || null,
+    select_data: searchPara.get("type") || null,
   });
 
-  // const courses = [
-  //   {
-  //     id: 1,
-  //     title: "Web Development for Beginners",
-  //     description:
-  //       "Kickstart your web development journey with this beginner-friendly course. Learn the fundamentals of HTML, CSS, and JavaScript, and build your first website. Ideal for those with no prior experience.",
-  //     categories: ["Web Technologies", "Front End", "IT"],
-  //     image: web,
-  //     star: "5.0",
-  //     price: "20.99",
-  //     isDiscount: [true, "30.99"],
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "UI/UX Design Essentials",
-  //     description:
-  //       "Master the art of creating user-centered designs. This course will guide you through the principles of UI/UX design, including wireframing, prototyping, and user testing, to ensure great user experiences.",
-  //     categories: ["UI/UX", "Front End", "DESIGN"],
-  //     image: ui,
-  //     star: "5.0",
-  //     price: "15.99",
-  //     isDiscount: [false, "40.99"],
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Cybersecurity Fundamentals",
-  //     description:
-  //       "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-  //     categories: ["Cybersecurity", "Security", "Computer Science"],
-  //     image: cyber,
-  //     star: "5.0",
-  //     price: "15.99",
-  //     isDiscount: [true, "40.99"],
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Cybersecurity Fundamentals",
-  //     description:
-  //       "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-  //     categories: ["Cybersecurity", "Security", "Computer Science"],
-  //     image: cyber,
-  //     star: "4.5",
-  //     price: "Free",
-  //     isDiscount: [false, "40.99"],
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Cybersecurity Fundamentals",
-  //     description:
-  //       "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-  //     categories: ["Cybersecurity", "Security", "Computer Science"],
-  //     image: cyber,
-  //     star: "4.0",
-  //     price: "15.99",
-  //     isDiscount: [true, "40.99"],
-  //   },
-  // ];
+  const [courses, setCourses] = useState([]);
+  const [lastNode, setLastNode] = useState(null);
+  const coursesContainer = useRef();
+  const { dataFetched, isFetching, refetch } = useInfiniteScroll({
+    fetchFn: (pagePara) => {
+      console.log("Page para", pagePara);
+      return useFetch(
+        `${API_BASE_URL}/getCourses`,
+        {
+          page: pagePara,
+          search_text: fetchData.search_text,
+          select_data: fetchData.select_data,
+        },
+        "POST"
+      );
+    },
+    queryKey: ["courses", fetchData.select_data],
+    scrollContainer: coursesContainer,
+    observedEle: lastNode,
+    data_id: "course_id",
+  });
+
+  const observedEle = (node) => {
+    setLastNode(node);
+  };
+  useEffect(() => {
+    console.log("Data fetched", dataFetched);
+    if (dataFetched) {
+      setCourses(dataFetched);
+    }
+  }, [dataFetched]);
 
   return (
     <div className="sm:px-5 px-1 height-vh-adjust flex items-center">
-      <div className=" bg-lightLayout dark:bg-lightDark py-10 overflow-auto h-[800px] flex-1">
+      <div
+        ref={coursesContainer}
+        className=" bg-lightLayout dark:bg-lightDark py-10 overflow-auto h-[800px] flex-1"
+      >
         <div className="md:px-5 px-2">
           <div className="flex items-center justify-between md:flex-row flex-col max-md:gap-5">
             <div className="font-[600] text-4xl">Courses</div>
@@ -83,15 +60,43 @@ function AllCourses() {
                 Filter
               </div>
               <input
+                onKeyDown={(e) => {
+                  if (isFetching) return;
+                  e.code === "Enter" ? refetch() : "";
+                }}
+                onChange={(e) => {
+                  setFetchData((prev) => ({
+                    ...prev,
+                    search_text: e.target.value,
+                  }));
+                  setSearchPara({
+                    search: e.target.value,
+                    type: searchPara.get("type") || null,
+                  });
+                }}
                 className="border dark:border-borderDark  lg:w-[500px] xl:w-[700px] w-full rounded-md"
                 type="text"
                 placeholder="search by name"
               />
               <select
+                onChange={(e) => {
+                  if (isFetching) return;
+                  setFetchData((prev) => ({
+                    ...prev,
+                    select_data: e.target.value,
+                  }));
+                  setSearchPara({
+                    search: searchPara.get("search") || null,
+                    type: e.target.value,
+                  });
+                }}
                 className="dark:bg-borderDark dark:border-none border  dark:text-white text-lightText focus:outline-none md:w-[200px] w-full p-2 text-lg appearance-none rounded-md"
                 name="courses"
                 id=""
               >
+                <option className="text-black" value={null}>
+                  Select Value
+                </option>
                 <option className="text-black" value={`Free`}>
                   Free
                 </option>
@@ -101,33 +106,36 @@ function AllCourses() {
                 <option className="text-black" value={`Discount`}>
                   Discount
                 </option>
-                <option className="text-black" value={`Top Rated`}>
+                <option className="text-black" value={`Top-Rated`}>
                   Top Rated
                 </option>
               </select>
               <FontAwesomeIcon
+                onClick={() => {
+                  if (isFetching) return;
+                  refetch();
+                }}
                 className="text-lg sm:px-3 cursor-pointer bg-black/20 rounded-sm py-3 max-sm:w-full"
                 icon={faMagnifyingGlass}
               />
             </div>
           </div>
           <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:gap-5 gap-8  mt-5">
-            {isLoading ? (
-              <Loader />
-            ) : !data?.msg?.length ? (
+            {!courses.length ? (
               <div className="flex justify-center xl:text-4xl text-xl font-bold absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
                 No courses are available
               </div>
             ) : (
-              data?.msg?.map((course) => (
+              courses.map((course) => (
                 <div
+                  ref={courses.at(-1) === course ? observedEle : null}
                   key={course.course_id}
                   className="border dark:border-borderDark rounded-xl bg-white dark:bg-dark"
                 >
                   <div>
                     <img
                       className="rounded-tr-xl rounded-tl-xl xl:w-[500px] w-full h-[300px] object-cover"
-                      src={`http://localhost:3002/uploads/${course.image_url}`}
+                      src={`${API_BASE_URL}/uploads/${course.image_url}`}
                       alt=""
                     />
                   </div>
@@ -141,9 +149,7 @@ function AllCourses() {
                           className="text-yellow-400"
                           icon={faStar}
                         />
-                        <div className="text-yellow-400">
-                          {course.evaluation}
-                        </div>
+                        <div className="text-yellow-400">{course.stars}</div>
                       </div>
                     </div>
                     <div className="text-black/50 leading-7 dark:text-white/50 mt-5 line-clamp-4  max-w-[400px]">
@@ -179,6 +185,14 @@ function AllCourses() {
                   </div>
                 </div>
               ))
+            )}
+
+            {isFetching && (
+              <>
+                <Loader />
+                <Loader />
+                <Loader />
+              </>
             )}
           </div>
         </div>
--- src/pages/Student/MyCourses.jsx
+++ src/pages/Student/MyCourses.jsx
@@ -1,65 +1,97 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import web from "../../assets/web.jpg";
 import ui from "../../assets/uiux.jpg";
 import cyber from "../../assets/cyber.jpg";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faArrowRight,
+  faMagnifyingGlass,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 import Button from "../../components/Button";
+import { Link, useSearchParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import useFetch from "../../hooks/useFetch";
+import API_BASE_URL from "../../config/config";
+import { UserData } from "../../context/UserDataContext";
+import Loader from "../../components/Loader";
 function MyCourses() {
-  const mycourses = [
-    {
-      id: 1,
-      title: "Web Development for Beginners",
-      description:
-        "Kickstart your web development journey with this beginner-friendly course. Learn the fundamentals of HTML, CSS, and JavaScript, and build your first website. Ideal for those with no prior experience.",
-      categories: ["Web Technologies", "Front End", "IT"],
-      image: web,
-      progress: "100%",
-      completed: true,
+  const [arr, setArr] = useState([]);
+
+  const [searchPara, setSearchPara] = useSearchParams();
+
+  const [currentPagePara, setCurrentPage] = useState(
+    Number(searchPara.get("page")) || 1
+  );
+
+  const [studentId, setStudentId] = useState();
+
+  const [fetchData, setFetchData] = useState({
+    page: currentPagePara,
+    search_text: searchPara.get("search") || null,
+    select_data: searchPara.get("type") || null,
+  });
+  const user_data = useContext(UserData);
+  useEffect(() => {
+    if (user_data) {
+      setStudentId(user_data.userData[0].student_id);
+    }
+  }, [user_data]);
+  const [courses, setCourses] = useState([]);
+  const [maxPage, setMaxPage] = useState(1);
+  const { data, isFetching, refetch } = useQuery({
+    queryFn: async () => {
+      if (!studentId) return;
+      return await useFetch(
+        `${API_BASE_URL}/getEnrolledCourses`,
+        {
+          page: currentPagePara,
+          student_id: studentId,
+          search_text: fetchData.search_text,
+          select_data: fetchData.select_data,
+        },
+        "POST"
+      );
     },
-    {
-      id: 2,
-      title: "UI/UX Design Essentials",
-      description:
-        "Master the art of creating user-centered designs. This course will guide you through the principles of UI/UX design, including wireframing, prototyping, and user testing, to ensure great user experiences.",
-      categories: ["UI/UX", "Front End", "DESIGN"],
-      image: ui,
-      progress: "70%",
-      completed: false,
-    },
-    {
-      id: 3,
-      title: "Cybersecurity Fundamentals",
-      description:
-        "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-      categories: ["Cybersecurity", "Security", "Computer Science"],
-      image: cyber,
-      progress: "40%",
-      completed: false,
-    },
-    {
-      id: 4,
-      title: "Cybersecurity Fundamentals",
-      description:
-        "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-      categories: ["Cybersecurity", "Security", "Computer Science"],
-      image: cyber,
-      progress: "60%",
-      completed: false,
-    },
-    {
-      id: 5,
-      title: "Cybersecurity Fundamentals",
-      description:
-        "Gain a solid foundation in cybersecurity. This course covers essential concepts such as network security, cryptography, threat detection, and best practices for protecting information systems. Perfect for beginners looking to start a career in cybersecurity",
-      categories: ["Cybersecurity", "Security", "Computer Science"],
-      image: cyber,
-      progress: "15%",
-      completed: false,
-    },
-  ];
+    queryKey: ["enrolled_courses", currentPagePara, fetchData.select_data],
+    enabled: !!studentId,
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    let tempArr = [];
+
+    let numberOfElemets = 5;
+
+    let value = currentPagePara / numberOfElemets;
+
+    let currentCycle = Math.ceil(value);
+
+    let maxNum = currentCycle * numberOfElemets;
+    let minNum = currentCycle * numberOfElemets - numberOfElemets;
+
+    let startValue = minNum + 1;
+
+    for (let i = startValue; i <= maxNum; i++) {
+      tempArr.push(i);
+    }
+
+    setArr(tempArr);
+  }, [currentPagePara]);
+
+  useEffect(() => {
+    if (data) {
+      const [currentCourses, [count]] = data?.msg;
+
+      setMaxPage(count["COUNT(*)"]);
+
+      setCourses(currentCourses);
+    }
+  }, [data]);
+
   return (
-    <div className="sm:px-5 px-1 height-vh-adjust flex items-center">
+    <div className="sm:px-5 height-vh-adjust px-1 flex items-center">
       <div className=" bg-lightLayout dark:bg-lightDark py-10 overflow-auto h-[800px] flex-1">
         <div className="md:px-5 px-2">
           <div className="flex items-center justify-between md:flex-row flex-col max-md:gap-5">
@@ -69,76 +101,213 @@ function MyCourses() {
                 Filter
               </div>
               <input
+                onKeyDown={(e) => {
+                  if (isFetching) return;
+                  e.code === "Enter" ? refetch() : "";
+                }}
+                onChange={(e) => {
+                  setFetchData((prev) => ({
+                    ...prev,
+                    search_text: e.target.value,
+                  }));
+                  setSearchPara({
+                    page: currentPagePara,
+                    search: e.target.value,
+                    type: searchPara.get("type") || null,
+                  });
+                }}
                 className="border dark:border-borderDark lg:w-[500px] xl:w-[700px] w-full rounded-md"
                 type="text"
                 placeholder="search by name"
               />
               <select
+                onChange={(e) => {
+                  if (isFetching) return;
+                  setFetchData((prev) => ({
+                    ...prev,
+                    select_data: e.target.value,
+                  }));
+                  setSearchPara({
+                    page: currentPagePara,
+                    search: searchPara.get("search") || null,
+                    type: e.target.value,
+                  });
+                }}
                 className="dark:bg-borderDark dark:border-none border  dark:text-white text-lightText focus:outline-none md:w-[200px] w-full p-2 text-lg appearance-none rounded-md"
                 name="courses"
                 id=""
               >
-                <option className="text-black" value={`Free`}>
+                <option className="text-black" value={null}>
+                  Select Value
+                </option>
+                <option className="text-black" value={"Completed"}>
                   Completed
                 </option>
-                <option className="text-black" value={`Top Rated`}>
+                <option className="text-black" value={`Top-Rated`}>
                   Top Rated
                 </option>
               </select>
               <FontAwesomeIcon
+                onClick={() => {
+                  if (isFetching) return;
+                  refetch();
+                }}
                 className="text-lg sm:px-3 cursor-pointer bg-black/20 rounded-sm py-3 max-sm:w-full"
                 icon={faMagnifyingGlass}
               />
             </div>
           </div>
+
           <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:gap-5 gap-8  mt-5">
-            {mycourses.map((course) => (
-              <div
-                key={course.id}
-                className="border dark:border-borderDark rounded-xl bg-white dark:bg-dark"
-              >
-                <div>
-                  <img
-                    className="rounded-tr-xl rounded-tl-xl xl:w-[500px] w-full h-[300px] object-cover"
-                    src={course.image}
-                    alt=""
-                  />
-                </div>
-                <div className="p-5">
-                  <div className="font-[600] lg:text-xl line-clamp-1">
-                    {course.title}
-                  </div>
-                  <div className="text-black/50 leading-7 dark:text-white/50 mt-5 line-clamp-4  max-w-[400px]">
-                    {course.description}
-                  </div>
-                  <div className="mt-5 flex gap-2 flex-wrap">
-                    {course.categories.map((category) => (
-                      <div className="bg-lightLayout min-w-10 text-center dark:bg-lightDark text-[13px] w-fit px-2 py-2 rounded-xl border dark:border-borderDark ">
-                        {category}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="flex items-center mt-5 gap-2">
-                    <div className=" w-full h-2 border border-borderDark rounded-md ">
-                      <div
-                        style={{ width: `${course.progress}` }}
-                        className={`bg-green-400 h-full rounded-md`}
-                      ></div>
-                    </div>
-                    <div className="">{course.progress}</div>
-                  </div>
-                  {course.completed && (
-                    <div className="flex items-center text-center gap-2 mt-5">
-                      <div className="bg-green-700/70 py-2 rounded-md flex-1 text-white">
-                        Completed !
-                      </div>
-                      <Button props="flex-1" margin="mt-0" text="Get Certificate" />
-                    </div>
-                  )}
-                </div>
+            {isFetching ? (
+              <>
+                <Loader />
+                <Loader />
+                <Loader />
+                <Loader />
+              </>
+            ) : courses.length === 0 ? (
+              <div className="flex justify-center xl:text-4xl text-xl font-bold absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+                No courses are available
               </div>
-            ))}
+            ) : (
+              courses.map((course) => (
+                <div
+                  key={course.course_id}
+                  className="border dark:border-borderDark rounded-xl bg-white dark:bg-dark"
+                >
+                  <div>
+                    <img
+                      className="rounded-tr-xl rounded-tl-xl xl:w-[500px] w-full h-[300px] object-cover"
+                      src={`${API_BASE_URL}/uploads/${course.image_url}`}
+                      alt="Image Course Preview"
+                    />
+                  </div>
+                  <div className="p-5">
+                    <div className="flex justify-between items-center">
+                      <div className="font-[600] lg:text-xl line-clamp-1">
+                        {course.title}
+                      </div>
+                      <div className="flex gap-2 items-center">
+                        <div className="dark:text-white text-md">{`${course.stars}.0`}</div>
+                        <FontAwesomeIcon
+                          className="text-yellow-400"
+                          icon={faStar}
+                        />
+                      </div>
+                    </div>
+                    <div className="text-black/50 leading-7 dark:text-white/50 mt-5 line-clamp-4  max-w-[400px]">
+                      {course.description}
+                    </div>
+                    <div className="mt-5 flex gap-2 flex-wrap">
+                      {course.tabs.split(",").map((category) => (
+                        <div
+                          key={Math.random()}
+                          className="bg-lightLayout min-w-10 text-center dark:bg-lightDark text-[13px] w-fit px-2 py-2 rounded-xl border dark:border-borderDark "
+                        >
+                          {category}
+                        </div>
+                      ))}
+                    </div>
+                    <div className="flex items-center mt-5 gap-2">
+                      <div className=" w-full h-2 border border-borderDark rounded-md ">
+                        <div
+                          style={{ width: `${course.progress}%` }}
+                          className={`bg-green-400 h-full rounded-md`}
+                        ></div>
+                      </div>
+                      <div className="">{course.progress}</div>
+                    </div>
+
+                    {Number(course.progress) === 100 ? (
+                      <div className="flex items-center text-center gap-2 mt-5">
+                        <div className="bg-green-700/70 py-2 rounded-md flex-1 text-white">
+                          Completed !
+                        </div>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    <Link
+                      to={`/student/CoursePage/${
+                        course.course_id
+                      }-${course.title.replace(/[\s/]/g, "")}`}
+                    >
+                      <Button text="Go to course" />
+                    </Link>
+                  </div>
+                </div>
+              ))
+            )}
           </div>
+          {Boolean(courses.length) && (
+            <div className="flex justify-center items-center gap-5 mt-10 ">
+              <FontAwesomeIcon
+                onClick={() => {
+                  if (!isFetching) {
+                    if (currentPagePara === 1) return;
+                    setSearchPara({
+                      page: currentPagePara - 1,
+                      search: fetchData.search_text,
+                      type: e.target.value,
+                    });
+                    setCurrentPage((prev) => prev - 1);
+                  }
+                }}
+                className={`bg-gray-500/20 p-4 rounded-md hover:bg-slate-400 cursor-pointer ${
+                  currentPagePara === 1
+                    ? "bg-gray-500/10 opacity-45 cursor-default hover:bg-gray-500/10"
+                    : ""
+                }`}
+                icon={faArrowLeft}
+              />
+              <div className="flex justify-center gap-4 text-lightText dark:text-white">
+                {arr.map((num) => {
+                  {
+                    return num > Math.ceil(maxPage / 4) ? (
+                      <></>
+                    ) : (
+                      <div
+                        onClick={() => {
+                          if (!isFetching) {
+                            setSearchPara({ page: Number(num) });
+                            setCurrentPage(Number(num));
+                          }
+                        }}
+                        key={num}
+                        className={`bg-gray-500/20 py-2 px-4 rounded-md hover:bg-slate-400 cursor-pointer ${
+                          (Number(searchPara.get("page")) || 1) === num
+                            ? "bg-purple-700 text-white"
+                            : ""
+                        }`}
+                      >
+                        {num}
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+              <FontAwesomeIcon
+                onClick={() => {
+                  if (!isFetching) {
+                    if (currentPagePara >= Math.ceil(maxPage / 4)) return;
+                    setSearchPara({
+                      page: currentPagePara + 1,
+                      search: searchPara.get("search") || null,
+                      type: e.target.value,
+                    });
+                    setCurrentPage((prev) => prev + 1);
+                  }
+                }}
+                className={`bg-gray-500/20 p-4 rounded-md hover:bg-slate-400 cursor-pointer ${
+                  Number(searchPara.get("page")) === Math.ceil(maxPage / 4)
+                    ? "bg-gray-500/10 opacity-45 cursor-default hover:bg-gray-500/10"
+                    : ""
+                }`}
+                icon={faArrowRight}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
