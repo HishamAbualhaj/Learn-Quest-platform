b43fcd3c7043bff3fe0e8fcd73c252dbe46258b4
HishamAbualhaj <hishamecommerce@gmail.com>
2025-05-01 21:11:04 +0300

    Infinite scroll data fetching for admin page (User,Course)

--- src/pages/Dashboard/Courses.jsx
+++ src/pages/Dashboard/Courses.jsx
@@ -1,37 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import DeleteCourse from "./DeleteCourse";
 import { Link } from "react-router-dom";
 import ButtonAdmin from "./ButtonAdmin";
 import useFetch from "../../hooks/useFetch";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import API_BASE_URL from "../../config/config";
 function Courses() {
   const [courses, setCourses] = useState([]);
+  const [lastNode, setLastNode] = useState(null);
+  const coursesContainer = useRef();
+  const { dataFetched, isFetching, hasNextPage } = useInfiniteScroll({
+    fetchFn: (pagePara) => {
+      return useFetch(`${API_BASE_URL}/getCourses`, { page: pagePara }, "POST");
+    },
+    queryKey: ["courses"],
+    scrollContainer: coursesContainer,
+    observedEle: lastNode,
+    data_id: "course_id",
+  });
 
   useEffect(() => {
-    (async () => {
-      const res = await useFetch(
-        "http://localhost:3002/getCourses",
-        null,
-        "GET"
-      );
-      let arrOfCourses = res.msg.map((data) => {
-        const { course_id, title, category, price, created_date } = data;
-        const new_date = created_date.split("T")[0];
-        return {
-          key: course_id,
-          name: title,
-          type: category,
-          price: price,
-          lessons: 0,
-          date: new_date,
-          action: ["Edit", "Delete"],
-        };
-      });
-      setCourses([...arrOfCourses]);
-    })();
-  }, []);
+    setCourses(dataFetched);
+  }, [dataFetched]);
+
+  const observeEle = (node) => {
+    setLastNode(node);
+  };
+
   const [deleteCoursePopup, setdeleteCoursePopup] = useState(false);
 
-  const [idCourse, setIdCourse] = useState(null);
+  const [courseData, setCourseData] = useState({});
 
   return (
     <>
@@ -50,69 +48,87 @@ function Courses() {
       </div>
       {deleteCoursePopup && (
         <DeleteCourse
-          setdeleteCoursePopup={setdeleteCoursePopup}
-          id={idCourse}
+          {...{
+            setdeleteCoursePopup: setdeleteCoursePopup,
+            id: courseData.id,
+            title: courseData.title,
+          }}
         />
       )}
-      <div className="xl:w-full lg:w-[850px] md:w-[600px] [450px]:w-[400px] w-[330px] mt-10 h-[600px] overflow-auto px-5">
-        <table className="dark:text-gray-300 text-lightText w-full">
+
+      <div
+        ref={coursesContainer}
+        className="xl:w-full lg:w-[850px] md:w-[600px] [450px]:w-[400px] w-[330px] mt-10 h-[650px] overflow-auto px-5"
+      >
+        <table className="dark:text-gray-300 text-lightText w-full table-auto">
           <thead>
             <tr className="border-t border-b dark:border-borderDark border-lightBorder">
-              <td className="py-4 font-bold">Name</td>
-              <td className="font-bold">Category</td>
-              <td className="whitespace-nowrap font-bold">Price</td>
-              <td className="font-bold">Lessons</td>
-              <td className="whitespace-nowrap xl:pr-0 pr-8 font-bold">
+              <th className="py-4 text-left font-bold">Name</th>
+              <th className="font-bold text-left">Category</th>
+              <th className="whitespace-nowrap font-bold text-left">Price</th>
+              <th className="font-bold text-left">Lessons</th>
+              <th className="whitespace-nowrap xl:pr-0 pr-8 font-bold text-left">
                 Date Created
-              </td>
-              <td className="font-bold">Action</td>
+              </th>
+              <th className="font-bold text-left">Action</th>
             </tr>
           </thead>
 
           <tbody>
             {courses.map((course) => (
               <tr
-                key={course.key}
+                ref={courses.at(-1) === course ? observeEle : null}
+                key={course?.course_id}
                 className="border-t border-b dark:border-borderDark border-lightBorder"
               >
                 <td className="py-4 xl:pr-0 pr-8 whitespace-nowrap">
-                  {course.name}
+                  {course?.title}
                 </td>
                 <td className="xl:pr-0 pr-8 whitespace-nowrap">
-                  {course.type}
+                  {course?.category}
                 </td>
                 <td className="xl:pr-0 pr-8 whitespace-nowrap">
-                  {course.price}
+                  {course?.price}
                 </td>
-                <td className="whitespace-nowrap">{course.lessons}</td>
-                <td className="whitespace-nowrap">{course.date}</td>
+                <td className="whitespace-nowrap">{0}</td>
+                <td className="whitespace-nowrap">
+                  {course?.created_date.split("T")[0]}
+                </td>
                 <td className="whitespace-nowrap p-2 flex flex-col gap-2">
-                  <Link to={`edit/${course.key}`} state={2}>
-                    <div
-                      id={course.key}
-                      onClick={() => {
-                        setIdCourse(course.key);
-                      }}
-                      className="cursor-pointer dark:bg-gray-500/70 bg-none dark:border-none border  py-2 px-2 text-center rounded-md dark:hover:bg-gray-800 hover:bg-gray-800 hover:text-white transition"
-                    >
-                      {course.action[0]}
+                  <Link to={`edit/${course?.course_id}`} state={2}>
+                    <div className="cursor-pointer dark:bg-gray-500/70 bg-none dark:border-none border  py-2 px-2 text-center rounded-md dark:hover:bg-gray-800 hover:bg-gray-800 hover:text-white transition">
+                      Edit
                     </div>
                   </Link>
                   <div
                     onClick={() => {
                       setdeleteCoursePopup(!deleteCoursePopup);
-                      setIdCourse(course.key);
+                      setCourseData({
+                        id: course?.course_id,
+                        title: course?.title,
+                      });
                     }}
-                    id={course.key}
-                    className="cursor-pointer border dark:border-red-500/70 border-red-300/70 dark:text-white text-lightText py-2 text-center rounded-md hover:bg-red-500/70 hover:text-white transition md:px-0 px-2 "
+                    className="cursor-pointer border dark:border-red-500/70 border-red-300/70 dark:text-white text-lightText py-2 px-2 text-center rounded-md hover:bg-red-500/70 hover:text-white transition lg:px-0"
                   >
-                    {course.action[1]}
+                    Delete
                   </div>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+
+        {isFetching ? (
+          <div className="dark:text-red-300 text-red-600 flex justify-center py-5 animate-syncPuls">
+            Loading ...
+          </div>
+        ) : (
+          !hasNextPage && (
+            <div className="dark:text-red-300 text-red-600 flex justify-center py-5">
+              No more courses
+            </div>
+          )
+        )}
       </div>
     </>
   );
--- src/pages/Dashboard/Users.jsx
+++ src/pages/Dashboard/Users.jsx
@@ -1,43 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DeleteUser from "./DeleteUser";
 import useFetch from "../../hooks/useFetch";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import API_BASE_URL from "../../config/config";
 function Users() {
- 
-
   const [users, setUsers] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const res = await useFetch("http://localhost:3002/getUsers", null, "GET");
-      let arrOfCourses = res.msg.map((data) => {
-        const {
-          student_id,
-          first_name,
-          last_name,
-          email,
-          joined_at,
-          role,
-          status_user,
-        } = data;
-        const date = joined_at.split("T")[0];
-        if (!(role === "admin")) {
-          return {
-            key: student_id,
-            name: `${first_name} ${last_name}`,
-            email: email,
-            date: date,
-            status: status_user ? "Active" : "Inactive",
-            courses: "10",
-            action: "Delete",
-          };
-        }
-      });
-      setUsers([...arrOfCourses]);
-    })();
-  }, []);
+  const [lastNode, setLastNode] = useState(null);
+  const usersContainer = useRef();
+  const { dataFetched, isFetching, hasNextPage } = useInfiniteScroll({
+    fetchFn: (pagePara) => {
+      return useFetch(`${API_BASE_URL}/getUsers`, { page: pagePara }, "POST");
+    },
+    queryKey: ["users"],
+    scrollContainer: usersContainer,
+    observedEle: lastNode,
+    data_id: "student_id",
+  });
 
+  useEffect(() => {
+    setUsers(dataFetched);
+  }, [dataFetched]);
+  const observeEle = (node) => {
+    setLastNode(node);
+  };
   const [deleteUserPopup, setdeleteUserPopup] = useState(false);
 
-  const [idUser, setIdUser] = useState(null);
+  const [userData, setUserData] = useState({});
 
   return (
     <>
@@ -48,9 +36,16 @@ function Users() {
         Last Login was yesterday at 2:46 pm
       </div>
       {deleteUserPopup && (
-        <DeleteUser setdeleteUserPopup={setdeleteUserPopup} id={idUser} />
+        <DeleteUser
+          {...{
+            setdeleteUserPopup: setdeleteUserPopup,
+            id: userData.id,
+            email: userData.email,
+          }}
+        />
       )}
-      <div className="xl:w-full lg:w-[850px] md:w-[600px] [450px]:w-[400px] w-[330px] mt-10 h-[650px] overflow-auto pr-5">
+     
+      <div ref={usersContainer} className="xl:w-full lg:w-[850px] md:w-[600px] [450px]:w-[400px] w-[330px] mt-10 overflow-auto pr-5">
         <table className="dark:text-gray-300 text-lightText w-full">
           <thead>
             <tr className="border-t border-b dark:border-borderDark border-borderLight">
@@ -66,51 +61,63 @@ function Users() {
           </thead>
 
           <tbody>
-            {users.map(
-              (user) =>
-                user && (
-                  <tr
-                    key={user?.key}
-                    className="border-t border-b dark:border-borderDark border-borderLight"
+            {users.map((user) => (
+              <tr
+                ref={users.at(-1) === user ? observeEle : null}
+                key={user?.student_id}
+                className="border-t border-b dark:border-borderDark border-borderLight"
+              >
+                <td className="py-4 xl:pr-0 pr-8 whitespace-nowrap">
+                  {`${user?.first_name} ${user?.last_name}`}
+                </td>
+                <td className="xl:pr-0 pr-8 whitespace-nowrap">
+                  {user?.email}
+                </td>
+                <td className="xl:pr-0 pr-8 whitespace-nowrap">
+                  {user?.joined_at.split("T")[0]}
+                </td>
+                <td className="xl:pr-0 pr-8 whitespace-nowrap">
+                  <div
+                    className={`py-2 px-3 rounded-lg w-fit ${
+                      user?.status_user
+                        ? "dark:text-green-300 text-green-400 bg-green-400/20"
+                        : "dark:text-red-300  text-red-400 bg-red-400/20"
+                    }`}
                   >
-                    <td className="py-4 xl:pr-0 pr-8 whitespace-nowrap">
-                      {user?.name}
-                    </td>
-                    <td className="xl:pr-0 pr-8 whitespace-nowrap">
-                      {user?.email}
-                    </td>
-                    <td className="xl:pr-0 pr-8 whitespace-nowrap">
-                      {user?.date}
-                    </td>
-                    <td className="xl:pr-0 pr-8 whitespace-nowrap">
-                      <div
-                        className={`py-2 px-3 rounded-lg w-fit ${
-                          user?.status === "Active"
-                            ? "dark:text-green-300 text-green-400 bg-green-400/20"
-                            : "dark:text-red-300  text-red-400 bg-red-400/20"
-                        }`}
-                      >
-                        {user?.status}
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap">{user?.courses}</td>
-                    <td className="whitespace-nowrap">
-                      <div
-                        onClick={() => {
-                          setdeleteUserPopup(!deleteUserPopup);
-                          setIdUser(user?.key);
-                        }}
-                        id={user?.key}
-                        className="cursor-pointer border dark:border-red-500/70 border-red-300/70 dark:text-white text-lightText py-2 md:px-0 px-2 text-center rounded-md hover:bg-red-500/70 hover:text-white transition"
-                      >
-                        {user?.action}
-                      </div>
-                    </td>
-                  </tr>
-                )
-            )}
+                    {user?.status_user ? "Active" : "Inactive"}
+                  </div>
+                </td>
+                <td className="whitespace-nowrap">10</td>
+                <td className="whitespace-nowrap">
+                  <div
+                    onClick={() => {
+                      setdeleteUserPopup(!deleteUserPopup);
+                      setUserData({
+                        id: user?.student_id,
+                        email: user?.email,
+                      });
+                    }}
+                    className="cursor-pointer border dark:border-red-500/70 border-red-300/70 dark:text-white text-lightText py-2 md:px-0 px-2 text-center rounded-md hover:bg-red-500/70 hover:text-white transition"
+                  >
+                    Delete
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
+
+        {isFetching ? (
+          <div className="dark:text-red-300 text-red-600 flex justify-center py-5 animate-syncPuls">
+            Loading ...
+          </div>
+        ) : (
+          !hasNextPage && (
+            <div className="dark:text-red-300 text-red-600 flex justify-center py-5">
+              No more users
+            </div>
+          )
+        )}
       </div>
     </>
   );
