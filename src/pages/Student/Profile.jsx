06c080b59c40e581265e5ca5e1df22836538ec2a
HishamAbualhaj <hishamecommerce@gmail.com>
2025-05-21 19:18:30 +0300

    Remove edit component outside main , use react query to handle fetching , handle image upload to be shown to user when upload new one, optimize functions creation

--- src/pages/Student/Profile.jsx
+++ src/pages/Student/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import Avatar from "../../components/Avatar";
 import { faGear, faUpload, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,6 +8,7 @@ import Alert from "../../components/Alert";
 import Person from "../../assets/person.png";
 import { UserData } from "../../context/UserDataContext";
 import API_BASE_URL from "../../config/config";
+import { useMutation } from "@tanstack/react-query";
 function Profile() {
   const [state, setState] = useState("Profile");
   const [isEdit, setIsEdit] = useState(false);
@@ -60,7 +61,7 @@ function Profile() {
     },
     {
       key: 7,
-      name: "Course Completed",
+      name: "Course Joined",
       data: "5",
     },
     {
@@ -96,7 +97,10 @@ function Profile() {
           ] = res.msg.data;
           image_url_temp = image_url;
 
-          const date = birthdate.split("T")[0];
+          let date = new Date(birthdate).toLocaleDateString("en-GB").split("/");
+
+          date = `${date[2]}-${date[1]}-${date[0]}`;
+
           const join = joined_at.split("T")[0];
 
           const values = [
@@ -172,179 +176,175 @@ function Profile() {
               )
             )
           ) : (
-            <EditProfile />
+            <EditProfile {...{ data_profile: data, userId, isEdit }} />
           )}
         </div>
       </div>
     </div>
   );
-  function EditProfile() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [file, setFile] = useState(null);
-    const [imageChange, setImageChange] = useState(false);
-    const [alert, setAlert] = useState(null);
+}
+function EditProfile({ data_profile, userId, isEdit }) {
+  const [file, setFile] = useState(null);
+  const [tempUrl, setTempUrl] = useState(null);
+  const [imageChange, setImageChange] = useState(false);
+  const [alert, setAlert] = useState(null);
 
-    const [dataProfile, setDataProfile] = useState(data);
+  const [dataProfile, setDataProfile] = useState(data_profile);
 
-    async function uploadImage() {
-      //Specific case for uploading image, (No need to manually set Content-Type)
-      const response = await fetch(`${API_BASE_URL}/handleUploads`, {
-        method: "POST",
-        body: file,
-      });
-      await response.json();
-    }
+  async function uploadImage() {
+    //Specific case for uploading image, (No need to manually set Content-Type)
+    const response = await fetch(`${API_BASE_URL}/handleUploads`, {
+      method: "POST",
+      body: file,
+    });
+    await response.json();
+  }
 
-    const handleChange = (e) => {
-      const { id, value } = e.target;
-      const newProfile = dataProfile.map((obj, index) => {
-        if (obj.key === Number(id)) {
-          // Change the value of current input (obj)
-          dataProfile[index] = {
-            ...dataProfile[index],
-            data: value,
-          };
-          return dataProfile[index];
-        } else {
-          return obj;
-        }
-      });
-      setDataProfile(newProfile);
-    };
-    useEffect(() => {
-      console.log("Data", dataProfile);
-    }, [dataProfile]);
-    const handleImageChange = (e) => {
-      const formdata = new FormData();
-      formdata.append("image", e.target?.files[0]);
-      formdata.append("id", userId);
-      setFile(formdata);
-
-      setImageChange(true);
-      dataProfile[7] = {
-        ...dataProfile[7],
-        data: e.target?.files[0].name,
-      };
-      setDataProfile(dataProfile);
-    };
-
-    async function handleProfileEdit() {
-      // setting data ready for server formate !
-      const obj = {
-        student_id: userId,
-        first_name: dataProfile[0].data,
-        last_name: dataProfile[1].data,
-        email: dataProfile[2].data,
-        birthdate: dataProfile[3].data,
-        image_url: dataProfile[7].data,
-        gender: dataProfile[4].data,
-        isImageChange: imageChange,
-      };
-
-      setIsLoading(true);
-
-      {
-        imageChange && (await uploadImage());
+  const handleChange = useCallback((e) => {
+    const { id, value } = e.target;
+    const newProfile = dataProfile.map((obj, index) => {
+      if (obj.key === Number(id)) {
+        // Change the value of current input (obj)
+        dataProfile[index] = {
+          ...dataProfile[index],
+          data: value,
+        };
+        return dataProfile[index];
+      } else {
+        return obj;
       }
+    });
+    setDataProfile(newProfile);
+  });
 
-      const res = await useFetch(`${API_BASE_URL}/updateUser`, obj, "PUT");
+  const handleImageChange = useCallback((e) => {
+    const formdata = new FormData();
+    formdata.append("image", e.target?.files[0]);
+    formdata.append("id", userId);
+    setFile(formdata);
 
-      setIsLoading(false);
-      setAlert(res);
+    setImageChange(true);
+    dataProfile[7] = {
+      ...dataProfile[7],
+      data: e.target?.files[0].name,
+    };
+    setDataProfile(dataProfile);
+    setTempUrl(URL.createObjectURL(e.target?.files[0]));
+  });
+
+  // setting data ready for server formate !
+  const obj = {
+    student_id: userId,
+    first_name: dataProfile[0].data,
+    last_name: dataProfile[1].data,
+    email: dataProfile[2].data,
+    birthdate: dataProfile[3].data,
+    image_url: dataProfile[7].data,
+    gender: dataProfile[4].data,
+    isImageChange: imageChange,
+  };
+
+  const { data, isPending, mutate } = useMutation({
+    mutationFn: async () => {
+      return await useFetch(`${API_BASE_URL}/updateUser`, obj, "PUT");
+    },
+  });
+  async function handleImageUpload() {
+    {
+      imageChange && (await uploadImage());
     }
+  }
 
-    return (
-      <div>
-        <div className="w-fit mx-auto">
-          <Avatar img={imageUrl} className="h-[250px] w-[250px]" />
-          <div className="relative  cursor-pointer ">
-            <div className="right-0 absolute text-white -top-14 shadow-custom text-xl dark:bg-lightDark bg-dark px-4 py-3 rounded-[100%]">
-              <FontAwesomeIcon icon={faUpload} />
-            </div>
-            <input
-              id="image"
-              onChange={handleImageChange}
-              type="file"
-              className="w-[52px] h-[52px] absolute right-0 -top-14 opacity-0"
-            />
+  useEffect(() => {
+    setAlert(data);
+  }, [data]);
+
+  return (
+    <div>
+      <div className="w-fit mx-auto">
+        <Avatar
+          img={
+            imageChange
+              ? tempUrl
+              : `${API_BASE_URL}/uploads/${dataProfile[7].data}`
+          }
+          className="h-[250px] w-[250px]"
+        />
+        <div className="relative  cursor-pointer ">
+          <div className="right-0 absolute text-white -top-14 shadow-custom text-xl dark:bg-lightDark bg-dark px-4 py-3 rounded-[100%]">
+            <FontAwesomeIcon icon={faUpload} />
           </div>
+          <input
+            id="image"
+            onChange={handleImageChange}
+            type="file"
+            className="w-[52px] h-[52px] absolute right-0 -top-14 opacity-0"
+          />
         </div>
+      </div>
 
-        {dataProfile.map((data) =>
-          data.key == 8 ? (
-            <></>
+      {dataProfile.map((data) =>
+        data.key == 8 ? (
+          <></>
+        ) : (
+          <div
+            key={data.key}
+            className="text-xl items-center flex gap-2 border dark:border-borderDark p-4 rounded-md mt-5"
+          >
+            {data.name}:
+            <div className="text-slate-300 flex-1">
+              {data.inType === "select" ? (
+                <select
+                  onChange={handleChange}
+                  className="w-full"
+                  id={data.key}
+                  value={data.data}
+                >
+                  <option className="text-black" value="Male">
+                    Male
+                  </option>
+                  <option className="text-black" value="Female">
+                    Female
+                  </option>
+                </select>
+              ) : (
+                <input
+                  id={data.key}
+                  onChange={handleChange}
+                  value={data.data || " "}
+                  className="border dark:border-borderDark w-full"
+                  type={data.inType}
+                  disabled={data.key === 6}
+                />
+              )}
+            </div>
+          </div>
+        )
+      )}
+      <div className="mt-5">
+        {alert &&
+          (alert?.status ? (
+            <Alert msg={alert?.msg} type="success" />
           ) : (
-            <div
-              key={data.key}
-              className="text-xl items-center flex gap-2 border dark:border-borderDark p-4 rounded-md mt-5"
-            >
-              {data.name}:
-              <div className="text-slate-300 flex-1">
-                {data.inType === "select" ? (
-                  <select
-                    onChange={handleChange}
-                    className="w-full"
-                    id={data.key}
-                  >
-                    {data.data === "Male" ? (
-                      <>
-                        <option className="text-black" value="Male">
-                          Male
-                        </option>
-                        <option className="text-black" value="Female">
-                          Female
-                        </option>
-                      </>
-                    ) : (
-                      <>
-                        <option className="text-black" value="Female">
-                          Female
-                        </option>
-                        <option className="text-black" value="Male">
-                          Male
-                        </option>
-                      </>
-                    )}
-                  </select>
-                ) : (
-                  <input
-                    id={data.key}
-                    onChange={handleChange}
-                    value={data.data || " "}
-                    className="border dark:border-borderDark w-full"
-                    type={data.inType}
-                    disabled={data.key === 6}
-                  />
-                )}
-              </div>
-            </div>
-          )
-        )}
-        <div className="mt-5">
-          {alert &&
-            (alert.status ? (
-              <Alert msg={alert.msg} type="success" />
-            ) : (
-              <Alert msg={alert.msg} type="failed" />
-            ))}
-        </div>
-        {isEdit &&
-          (isLoading ? (
-            <div>
-              <ButtonAdmin text="LOADING ... " />
-            </div>
-          ) : (
-            <div
-              onClick={() => {
-                handleProfileEdit();
-              }}
-            >
-              <ButtonAdmin text="Edit Profile" />
-            </div>
+            <Alert msg={alert?.msg} />
           ))}
       </div>
-    );
-  }
+      {isEdit &&
+        (isPending ? (
+          <div>
+            <ButtonAdmin text="LOADING ... " />
+          </div>
+        ) : (
+          <div
+            onClick={() => {
+              mutate();
+              handleImageUpload();
+            }}
+          >
+            <ButtonAdmin text="Edit Profile" />
+          </div>
+        ))}
+    </div>
+  );
 }
-
 export default Profile;
