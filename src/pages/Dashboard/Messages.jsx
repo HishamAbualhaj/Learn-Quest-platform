2e76072528e6be6051b71098ffbb4645669bfa1c
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-05 19:14:50 +0300

    Create messages component  to both for user and admin

--- src/pages/Dashboard/Messages.jsx
+++ src/pages/Dashboard/Messages.jsx
@@ -1,24 +1,169 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import useFetch from "../../hooks/useFetch";
+import API_BASE_URL from "../../config/config";
+import Avatar from "../../components/Avatar";
 
-function Messages(student_id) {
+function Messages({
+  msg_id,
+  sender_id,
+  receiver_id,
+  msg,
+  date,
+  currentUserId,
+  sender_name,
+  sender_image,
+  receiver_name,
+  receiver_image,
+}) {
+  const [messages, setMessages] = useState([]);
+  const [lastNode, setLastNode] = useState(null);
+
+  const msgContainer = useRef(null);
+
+  const { dataFetched, isFetching, refetch } = useInfiniteScroll({
+    fetchFn: async (pagePara) => {
+      if (!sender_id && !receiver_id) return;
+      return await useFetch(
+        `${API_BASE_URL}/getMsg`,
+        { page: pagePara, ...{ sender_id, receiver_id } },
+        "POST"
+      );
+    },
+    queryKey: ["messages", receiver_id],
+    scrollContainer: msgContainer,
+    observedEle: lastNode,
+    data_id: "msg_id",
+  });
+
+  useEffect(() => {
+    setMessages([]);
+    if (receiver_id && !isFetching) {
+      refetch();
+    }
+  }, [receiver_id]);
+
+  useEffect(() => {
+    if (msg) {
+      setMessages((prev) => [
+        ...prev,
+        {
+          msg_id: msg_id,
+          sender_id: sender_id,
+          receiver_id: receiver_id,
+          msg_text: msg,
+          created_date: date,
+        },
+      ]);
+    }
+  }, [msg_id]);
+  const prevScrollHeight = useRef(0);
+  useEffect(() => {
+    const arr = [...dataFetched];
+    setMessages(arr.reverse());
+    prevScrollHeight.current = msgContainer.current.scrollHeight;
+  }, [dataFetched]);
+
+  useEffect(() => {
+    if (msgContainer.current && messages.length <= 10) {
+      msgContainer.current.scrollTop = msgContainer.current.scrollHeight;
+    } else {
+      const container = msgContainer.current;
+      container.scrollTop = container.scrollHeight - prevScrollHeight.current;
+    }
+  }, [messages]);
+
+  const observeEle = (node) => {
+    setLastNode(node);
+  };
+
+  function formatDateForUX(dateStr) {
+    const date = new Date(dateStr);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true, // For AM/PM format
+    };
+
+    return date.toLocaleString("en-US", options);
+  }
   return (
-    <div className="flex flex-col p-5 gap-5 overflow-auto">
-      {[].map((message) => (
-        <div
-          className={`flex flex-col ${isAdmin ? "items-start" : "items-end"} `}
-        >
-          <div className="dark:text-white text-lightText text-md ">{name}</div>
-          <div
-            className={`flex lg:w-1/2 w-fit ${
-              isAdmin ? "dark:bg-blue-400/40 bg-blue-500/90" : "bg-gray-700"
-            }  p-2 rounded-md mt-1`}
-          >
-            <div className="text-white text-start">{text}</div>
+    <div ref={msgContainer} className="flex flex-col p-5 gap-5 overflow-auto">
+      {isFetching && (
+        <>
+          <div className="flex flex-col">
+            <div className="flex lg:w-1/2 w-fit h-[40px] bg-gray-700 animate-syncPuls p-2 rounded-md mt-1"></div>
           </div>
-          <div className="mt-2 text-sm text-gray-400">25 Oct 3:20 PM</div>
+          <div className="flex flex-col items-end">
+            <div className="flex lg:w-1/2 w-fit h-[40px] bg-gray-700 p-2 animate-syncPuls rounded-md mt-1"></div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex lg:w-1/2 w-fit h-[40px] bg-gray-700 animate-syncPuls p-2 rounded-md mt-1"></div>
+          </div>
+          <div className="flex flex-col items-end">
+            <div className="flex lg:w-1/2 w-fit h-[40px] bg-gray-700 p-2 animate-syncPuls rounded-md mt-1"></div>
+          </div>
+        </>
+      )}
+
+      {receiver_id ? (
+        !isFetching &&
+        (messages.length === 0 ? (
+          <div className="text-xl flex justify-center dark:text-white text-lightDark">
+            No Chat data for this user
+          </div>
+        ) : (
+          <></>
+        ))
+      ) : (
+        <div className="text-2xl flex justify-center dark:text-white text-lightDark">
+          Select Chat
+        </div>
+      )}
+      {messages.map((message) => (
+        <div
+          key={message?.msg_id}
+          ref={messages[0] === message ? observeEle : null}
+          className={`flex flex-col ${
+            message?.sender_id === currentUserId ? "items-start" : "items-end"
+          } `}
+        >
+          <div className="dark:text-white text-lightText text-sm">
+            {message?.sender_id === currentUserId ? sender_name : receiver_name}
+          </div>
+          <div
+            className={`flex mt-2 items-center gap-2 ${
+              message?.sender_id === currentUserId ? "" : "flex-row-reverse"
+            }`}
+          >
+            <Avatar
+              img={
+                message?.sender_id === currentUserId
+                  ? sender_image
+                  : receiver_image
+              }
+              className={`h-[40px] w-[40px]`}
+            />
+            <div
+              className={`flex flex-1 lg:w-1/2 w-fit ${
+                message?.sender_id === currentUserId
+                  ? "dark:bg-blue-400/40 bg-blue-500/90"
+                  : "bg-gray-700"
+              }  p-2 rounded-md mt-1`}
+            >
+              <div className="text-white text-start">{message?.msg_text}</div>
+            </div>
+          </div>
+          <div className="mt-2 text-sm text-gray-400">
+            {formatDateForUX(message?.created_date)}
+          </div>
         </div>
       ))}
     </div>
   );
 }
+
 export default Messages;
