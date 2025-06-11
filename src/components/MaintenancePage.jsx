5d5fd66603f410ea2c341d1e075c0c768bae1cf1
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-11 16:25:54 +0300

    Mainteance fallback while system is down

---
+++ src/components/MaintenancePage.jsx
@@ -0,0 +1,21 @@
+import { faHammer } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function MaintenancePage() {
+  return (
+    <div className="dark:bg-dark bg-slate-300 h-screen dark:text-white flex items-center justify-center">
+      <div className="flex flex-col items-center gap-5">
+        <FontAwesomeIcon
+          icon={faHammer}
+          className="text-[80px] text-purple-500/50 animate-bounce"
+        />
+        <div className="flex flex-col gap-2 items-center">
+          <div className="text-3xl uppercase font-bold">Website is under maintenance</div>
+          <div className="text-2xl uppercase">We'll be back shortly</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MaintenancePage;
