37ef8f917fe3cac150baed0f06959c773616da6f
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-11 16:32:51 +0300

    Remove unwanted imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ThemeContext from "./context/ThemeContext.jsx";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
-
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
