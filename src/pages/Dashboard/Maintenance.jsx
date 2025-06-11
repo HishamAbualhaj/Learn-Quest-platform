0df4d94c8a95272f9b0cc837396d639262d51167
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-11 16:29:13 +0300

    Handle maintenance actions for admin

--- src/pages/Dashboard/Maintenance.jsx
+++ src/pages/Dashboard/Maintenance.jsx
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useFetch from "../../hooks/useFetch";
+import API_BASE_URL from "../../config/config";
+import { UserData } from "../../context/UserDataContext";
+import Alert from "../../components/Alert";
 export default function Maintenance() {
-  let status = "Active";
-  let isActive = true;
+  const data_user = useContext(UserData);
+  const [userData, setUserData] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [msg, setMsg] = useState(null);
+  useEffect(() => {
+    if (data_user) {
+      const userDataArray = data_user?.userData;
+      const { student_id, email, first_name } = userDataArray[0];
+      setUserData({ student_id, email, first_name });
+    }
+  }, [data_user]);
+
+  const { data, refetch } = useQuery({
+    queryKey: ["maintenance"],
+    queryFn: async () => {
+      return await useFetch(`${API_BASE_URL}/getMaintenace`, null, "GET");
+    },
+  });
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      return await useFetch(
+        `${API_BASE_URL}/setMaintenance`,
+        { ...userData, status },
+        "POST"
+      );
+    },
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  useEffect(() => {
+    if (data) {
+      const [{ status }] = data?.msg;
+      setAction(false);
+      setMsg(
+        `${status ? "System is running now" : "System is down for maintenance"}`
+      );
+      setStatus(status);
+    }
+  }, [data]);
+
+  const [count, setCount] = useState(5);
+  const [action, setAction] = useState(false);
+  useEffect(() => {
+    let countTimer = null;
+    if (action) {
+      setCount(5);
+      countTimer = setInterval(() => {
+        setCount((prev) => {
+          if (prev === 0) {
+            clearInterval(countTimer);
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(countTimer);
+  }, [action]);
+
   return (
     <div>
+      {msg === null ? (
+        <></>
+      ) : (
+        <Alert
+          type={`${action ? "fail" : "success"}`}
+          msg={`${action ? `${msg} ${count} sec` : msg}`}
+        />
+      )}
       <div className="items-center sm:flex block justify-between">
         <div className="title">
           <div className="dark:text-white text-lightText font-semibold text-4xl">
@@ -15,25 +85,50 @@ export default function Maintenance() {
             Shut down the server for maintenance purposes
           </div>
         </div>
-        <div
-          className={`py-2 px-6 rounded-lg text-center text-2xl sm:mt-0 mt-5 sm:w-fit w-full ${
-            status === "Active"
-              ? "dark:text-green-300 text-green-400 bg-green-400/20"
-              : "dark:text-red-300  text-red-400 bg-red-400/20"
-          }`}
-        >
-          {status}
-        </div>
+        {status === null ? (
+          <div
+            className={`py-2 px-6 rounded-lg text-center text-4xl sm:mt-0 mt-5 sm:w-fit w-fulldark:text-red-300  text-red-400 bg-red-400/20"`}
+          >
+            ...
+          </div>
+        ) : (
+          <div
+            className={`py-2 px-6 rounded-lg text-center text-2xl sm:mt-0 mt-5 sm:w-fit w-full ${
+              status
+                ? "dark:text-green-300 text-green-400 bg-green-400/20"
+                : "dark:text-red-300  text-red-400 bg-red-400/20"
+            }`}
+          >
+            {status ? "Active" : "Inactive"}
+          </div>
+        )}
       </div>
       <div className="flex  justify-center items-center min-h-[500px] dark:bg-black/20 bg-black/10 mt-5">
-        <FontAwesomeIcon
-          className={`cursor-pointer  text-[200px] ${
-            isActive
-              ? "dark:text-red-400/40 text-red-300 dark:hover:text-red-500 hover:text-red-500"
-              : "dark:text-green-400/40 text-green-300 dark:hover:text-green-500 hover:text-green-500"
-          }  transition`}
-          icon={faPowerOff}
-        />
+        {status === null ? (
+          <></>
+        ) : (
+          !isPending && (
+            <FontAwesomeIcon
+              onClick={() => {
+                setAction(true);
+                setMsg(
+                  `${
+                    status
+                      ? "System will shut down after"
+                      : "System will run after"
+                  }`
+                );
+                mutate();
+              }}
+              className={`cursor-pointer  text-[200px] ${
+                status
+                  ? "dark:text-red-400/40 text-red-300 dark:hover:text-red-500 hover:text-red-500"
+                  : "dark:text-green-400/40 text-green-300 dark:hover:text-green-500 hover:text-green-500"
+              }  transition`}
+              icon={faPowerOff}
+            />
+          )
+        )}
       </div>
     </div>
   );
