dbf106431999f2accf244efe79bf198553697e06
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-23 16:28:44 +0300

    Check if blob url then put it without base url

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,14 @@
 import API_BASE_URL from "../config/config";
 import Person from "../assets/person.png";
 import { useEffect } from "react";
-function Avatar({ img, className }) {
-  return (
+function Avatar({ img, className, isBlob = false }) {
+  return isBlob ? (
+    <img
+      className={`rounded-[50%] object-cover ${className}`}
+      src={img}
+      alt=""
+    />
+  ) : (
     <img
       className={`rounded-[50%] object-cover ${className}`}
       src={`${img ? `${API_BASE_URL}/uploads/${img}` : Person}`}
