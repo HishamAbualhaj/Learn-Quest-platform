34b2d6163be28ee4b0017949b57a9f5110945ea2
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-23 16:22:14 +0300

    Add comments to show how the method work

--- src/hooks/useArrange.js
+++ src/hooks/useArrange.js
@@ -14,4 +14,58 @@ function useArrange(data, id) {
       ];
 }
 
+// *** first step ***
+
+// [
+//   {
+//     status: true,
+//     msg: [{ student_id: 1 }, { student_id: 2 }, { student_id: 3 }],
+//     redirect: true,
+//   },
+//   {
+//     status: true,
+//     msg: [{ student_id: 4 }, { student_id: 5 }, { student_id: 6 }],
+//     redirect: true,
+//   },
+//   {
+//     status: true,
+//     msg: [{ student_id: 7 }, { student_id: 8 }, { student_id: 9 }],
+//     redirect: true,
+//   },
+// ];
+
+
+// *** second step ***
+
+// [
+//   [{ student_id: 1 }, { student_id: 2 }, { student_id: 3 }],
+//   [{ student_id: 4 }, { student_id: 5 }, { student_id: 6 }],
+//   [{ student_id: 7 }, { student_id: 8 }, { student_id: 9 }],
+// ];
+
+
+// *** third step ***
+
+// [
+//   { student_id: 1 },
+//   { student_id: 2 },
+//   { student_id: 3 },
+//   { student_id: 4 },
+//   { student_id: 5 },
+//   { student_id: 6 },
+//   { student_id: 7 },
+//   { student_id: 8 },
+//   { student_id: 9 },
+// ];
+
+
+// *** last step for Map ***
+// key -> value to avoid duplication
+
+// [
+//   [1, { student_id: 1 }],
+//   [2, { student_id: 2 }],
+//   [3, { student_id: 3 }],
+//   [4, { student_id: 4 }],
+// ];
 export default useArrange;
