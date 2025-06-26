1682adafdfa6bf0ffd75af14f068a0a97e493bbc
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-26 18:40:58 +0300

    remove / from back url coming from env vercel

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const BACKEND_URL_DEVELOPMENT =
   import.meta.env.VITE_BACKEND_URL_DEVELOPMENT || "http://localhost:3002";
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? BACKEND_URL_PRODUCTION
+    ? BACKEND_URL_PRODUCTION.replace(/\/$/, "")
     : BACKEND_URL_DEVELOPMENT;
 
 export default API_BASE_URL;
