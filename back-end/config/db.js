0bd8eaff56237d164b506a7a6331f5663e5e4278
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-26 17:50:53 +0300

    Add env for db config

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,11 +1,17 @@
 import mysql from "mysql2";
-
+import dotenv from "dotenv";
+dotenv.config();
+const dbPassword = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "learnquest",
-  port: 3308
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+  port: dbPort,
 });
 
 connection.connect((err) => {
