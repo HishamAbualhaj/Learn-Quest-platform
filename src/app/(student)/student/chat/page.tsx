33f1df05d5427b97dc068b7b60a67cfe4a93c26e
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-11-05 07:53:37 +0200

    add route to chat

---
+++ src/app/(student)/student/chat/page.tsx
@@ -0,0 +1,24 @@
+import API_BASE_URL from "@/config/config";
+import useFetchServer from "@/hooks/useFetchServer";
+import getSession from "@/lib/getSession";
+import ChatStudent from "@/pages/Student/ChatStudent";
+import { User } from "@/types";
+
+const page = async () => {
+  const { userDataServer } = await getSession();
+  const adminData = await useFetchServer<User>(
+    `${API_BASE_URL}/getAdminId`,
+    null,
+    "GET"
+  );
+
+
+  return (
+    <ChatStudent
+      userData={userDataServer[0]}
+      adminData={adminData.msg[0] as User}
+    />
+  );
+};
+
+export default page;
