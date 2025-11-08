8f6b39ed67b0bfaeab3768674ffd8b48b6be503e
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-11-08 21:31:24 +0200

    Assign values to db from env file

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,18 +1,20 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
-// const dbPassword = process.env.DB_PASSWORD;
-// const dbUser = process.env.DB_USER;
-// const dbHost = process.env.DB_HOST;
-// const dbPort = process.env.DB_PORT;
-// const dbName = process.env.DB_NAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Kalousha2001#",
-  database: "learnquest",
-  port: 3306,
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+  port: dbPort,
 });
+
 
 connection.connect((err) => {
   if (err) throw err;
