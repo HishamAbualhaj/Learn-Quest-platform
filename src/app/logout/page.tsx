8f2f9c9a4990704e7ed251242d4ce88c971f73ae
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-11-09 19:47:32 +0200

    assign /logout force-dynamic

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,6 +1,9 @@
 import API_BASE_URL from "@/config/config";
 import useFetchServer from "@/hooks/useFetchServer";
 import Logout from "@/pages-content/auth/Logout";
+
+export const dynamic = "force-dynamic";
+
 const page = async () => {
   await useFetchServer(`${API_BASE_URL}/logout`, null, "GET");
   return <Logout />;
