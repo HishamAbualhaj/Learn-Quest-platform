139add688db2fec782e44542c91c49316c909a5f
Hisham Abualhaj <hishamecommerce@gmail.com>
2026-08-17 01:48:25 +0300

    feat: make the API same-site with the frontend

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,7 @@ const BACKEND_URL_PRODUCTION =
 const BACKEND_URL_DEVELOPMENT =
   process.env.NEXT_PUBLIC_BACKEND_URL_DEVELOPMENT || "http://localhost:3002";
 
-const API_BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? BACKEND_URL_PRODUCTION.replace(/\/$/, "")
-    : BACKEND_URL_DEVELOPMENT;
+const API_BASE_URL = "/backend"
 
 const BACKEND_WEBSOCKET_DEVELOPMENT =
   process.env.NEXT_PUBLIC_BACKEND_WEBSOCKET_DEVELOPMENT;
