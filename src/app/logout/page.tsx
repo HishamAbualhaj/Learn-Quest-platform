53d1115939fbdddbb81ed97641408f67e25bcf0a
Hisham Abualhaj <hishamecommerce@gmail.com>
2026-08-17 02:52:48 +0300

    fix: refactor logout to client component

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,11 +1,6 @@
-"use client";
-import API_BASE_URL from "@/config/config";
-import useFetch from "@/hooks/useFetch";
-
 import Logout from "@/pages-content/auth/Logout";
 
-const page = async () => {
-  await useFetch(`${API_BASE_URL}/logout`, null, "GET");
+const page = () => {
   return <Logout />;
 };
 
--- src/pages-content/auth/Logout.tsx
+++ src/pages-content/auth/Logout.tsx
@@ -1,12 +1,22 @@
 "use client";
+import API_BASE_URL from "@/config/config";
+import useFetch from "@/hooks/useFetch";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function Logout() {
   const router = useRouter();
+  const hasRun = useRef(false);
+
   useEffect(() => {
-    router.push("/");
+    if (hasRun.current) return;
+    hasRun.current = true;
+
+    useFetch(`${API_BASE_URL}/logout`, null, "GET").finally(() => {
+      router.push("/");
+    });
   }, [router]);
+
   return null;
 }
 
